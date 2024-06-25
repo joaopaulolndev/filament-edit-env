c34b930e2e3c7dfc2791ef878e7122b9a3a3aa2b
Joao Nascimento <joao.nascimento.ext@tlscontact.com>
2024-06-25 20:17:43 +0100

    Add pt language

---
+++ resources/lang/pt/default.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'heading' => 'Editar arquivo .env',
+    'save' => 'Arquivo .env salvo com sucesso.',
+    'hint' => 'Este plugin permite editar o arquivo .env. Tome cuidado! (não funciona em produção)',
+];
